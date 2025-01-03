1b061ade686fe0facbb5ba5f2a667094ec132a57
Anmol Garg <anmolgarg271005@gmail.com>
2025-01-03 22:30:29 +0530

    Improve websockets logic

--- src/SocketController/socketController.ts
+++ src/SocketController/socketController.ts
@@ -2,6 +2,7 @@ import { Socket } from "socket.io";
 import { USERS } from "../state/UserState";
 import { createBoard } from "../utils/CreateBoard";
 import { BOARDS, Player } from "../state/BoardState";
+import { CheckWinner } from "../utils/CheckWinner";
 
 export const socketController = (socket: Socket) => { 
 
@@ -9,7 +10,6 @@ export const socketController = (socket: Socket) => {
     USERS.set(socket.id ,{ boardId: null, findMatch: false });
     
     socket.on("findMatch", () => {
-        console.log("User finding match", socket.id)
         
         USERS.get(socket.id)!.findMatch = true;
 
@@ -18,8 +18,6 @@ export const socketController = (socket: Socket) => {
                 const boardId = createBoard(socket.id, socketId)
                 socket.emit("matchFound", boardId)
                 socket.to(socketId).emit("matchFound", boardId)
-                socket.emit("yourTurn", true);
-                socket.to(socketId).emit("yourTurn", false);
             }
         })
     })
@@ -38,14 +36,33 @@ export const socketController = (socket: Socket) => {
         board.board[data.row][data.col] = board.symbols[socket.id];
 
         socket.emit("moveMade", { row: data.row, col: data.col, symbol: board.symbols[socket.id] });
-        socket.emit("yourTurn", false);
-
+        
         const opponentId = board.turn === Player.user1 ? board.user2 : board.user1;
         socket.broadcast.to(opponentId).emit("moveMade", { row: data.row, col: data.col, symbol: board.symbols[socket.id] });
+        
+        // CheckWinner();
+
+        socket.emit("yourTurn", false);
         socket.broadcast.to(opponentId).emit("yourTurn", true);
 
         board.turn = board.turn === Player.user1 ? Player.user2 : Player.user1;
     });
+
+    socket.on("StartGame", (boardId: string) => {
+        console.log("StartGame", boardId)
+        const board = BOARDS.get(boardId);
+        console.log(board)
+        if (!board) return;
+        
+        if (board.turn === Player.user1 && socket.id === board.user1) {
+            socket.emit("yourTurn", true);
+            socket.to(board.user2).emit("yourTurn", false);
+        }
+        else if (board.turn === Player.user2 && socket.id === board.user2) { 
+            socket.emit("yourTurn", true);
+            socket.to(board.user1).emit("yourTurn", false);
+        }
+    })
 
     socket.on('disconnect', () => {
         console.log("User disconnected", socket.id)
