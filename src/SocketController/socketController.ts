0c758b18e58be631bfe700cc8c5397f87c3704a1
Anmol Garg <anmolgarg271005@gmail.com>
2025-01-05 22:08:27 +0530

    Add play again logix

--- src/SocketController/socketController.ts
+++ src/SocketController/socketController.ts
@@ -45,10 +45,7 @@ export const socketController = (socket: Socket , io:Server) => {
         const opponentId = board.turn === Player.user1 ? board.user2 : board.user1;
         socket.broadcast.to(opponentId).emit("moveMade", { row: data.row, col: data.col, symbol: board.symbols[socket.id] });
         
-        if (isGameFinish(socket, io, board)) {
-            
-        }
-        else {   
+        if (!isGameFinish(socket, io, board)) {
             socket.emit("yourTurn", false);
             socket.broadcast.to(opponentId).emit("yourTurn", true);
             
@@ -59,15 +56,68 @@ export const socketController = (socket: Socket , io:Server) => {
     socket.on("StartGame", (boardId: string) => {
         const board = BOARDS.get(boardId);
         if (!board) return;
+
+        io.to(board.user1).emit("symbol", board.symbols[board.user1]);
+        io.to(board.user2).emit("symbol", board.symbols[board.user2]);
+        console.log(board.symbols[board.user1])
+        console.log(board.symbols[board.user2])
         
         if (board.turn === Player.user1 && socket.id === board.user1) {
             socket.emit("yourTurn", true);
             socket.to(board.user2).emit("yourTurn", false);
         }
-        else if (board.turn === Player.user2 && socket.id === board.user2) { 
+        else if (board.turn === Player.user2 && socket.id === board.user2) {
             socket.emit("yourTurn", true);
             socket.to(board.user1).emit("yourTurn", false);
         }
+    })
+
+    socket.on("askPlayAgain", (boardId:string) => {
+        const board = BOARDS.get(boardId);
+        if (!board) return;
+        const opponentId = board.user1 === socket.id ? board.user2 : board.user1;
+
+        io.to(opponentId).emit("wantToPlayAgain");
+    })
+
+    socket.on("yesPlayAgain", (boardId: string) => {
+        const board = BOARDS.get(boardId);
+        if (!board) return;
+        
+        board.symbols[board.user1] = board.symbols[board.user1] === "X" ? "O" : "X";
+        board.symbols[board.user2] = board.symbols[board.user2] === "X" ? "O" : "X";
+        board.board = Array(3).fill(null).map(() => Array(3).fill(null));
+        board.turn = board.symbols[board.user1] === "X" ? Player.user1 : Player.user2;
+
+        io.to(board.user1).emit("symbol", board.symbols[board.user1]);
+        io.to(board.user2).emit("symbol", board.symbols[board.user2]);
+
+        io.to(board.user1).emit("playAgain");
+        io.to(board.user2).emit("playAgain");
+
+        if(board.turn === Player.user1) {
+            io.to(board.user1).emit("yourTurn", true);
+            io.to(board.user2).emit("yourTurn", false);
+        }
+        else {
+            io.to(board.user1).emit("yourTurn", false);
+            io.to(board.user2).emit("yourTurn", true);
+        }
+    });
+
+    socket.on("noPlayAgain", (boardId: string) => { 
+        const board = BOARDS.get(boardId);
+        if (!board) return;
+
+        USERS.get(board.user1)!.boardId = null;
+        USERS.get(board.user1)!.findMatch = false;
+        USERS.get(board.user2)!.boardId = null;
+        USERS.get(board.user2)!.findMatch = false;
+
+        BOARDS.delete(boardId);
+
+        io.to(board.user1).emit("noPlayAgain");
+        io.to(board.user2).emit("noPlayAgain");
     })
 
     socket.on('disconnect', () => {
